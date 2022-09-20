5dd846792bf4b73199377eb63673cbb4d3d7e4c8
Hjalmar <hjalmar.noren@edu.lycksele.se>
2022-09-20 13:50:12 +0200

    p1 start

---
+++ Projekt/P1 - BigFoot/assets/js/bigfoot.js
@@ -0,0 +1,8 @@
+function loadBigfoot() {
+    document.getElementById("bigFoot").src ="assets/images/bigfoot.png";
+
+}
+
+function moveBigfoot(){
+    var picture = document.getElementById("bigFoot")
+}
