5b34e87cfe1f6f9a60a98119018e760c65043574
Hjalmar <hjalmar.noren@edu.lycksele.se>
2022-09-22 11:57:06 +0200

    p1 uppdatering

--- Projekt/P1 - BigFoot/assets/js/bigfoot.js
+++ Projekt/P1 - BigFoot/assets/js/bigfoot.js
@@ -4,5 +4,17 @@ function loadBigfoot() {
 }
 
 function moveBigfoot(){
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    alert ('Du hittade Bigfoot!')
     var picture = document.getElementById("bigFoot")
+    var y = Math.random() * height
+    var x = Math.random() * width
+
+
+    picture.style.top = y + "px"
+    picture.style.left = x + "px"
+
+    console.log(width, height);
 }
+
