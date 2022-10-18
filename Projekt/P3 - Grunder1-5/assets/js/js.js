3370fd29f546b51e19e5136c686b7047b1d581c0
Hjalmar <hjalmar.noren@edu.lycksele.se>
2022-10-18 13:53:07 +0200

    Added 2-5

---
+++ Projekt/P3 - Grunder1-5/assets/js/js.js
@@ -0,0 +1,8 @@
+function button(){
+   document.getElementById("input1") = document.getElementById("input2")
+   alert("Hello! I am an alert box!!");
+   
+
+} 
+
+document.getElementById("button").addEventListener("click", button);
